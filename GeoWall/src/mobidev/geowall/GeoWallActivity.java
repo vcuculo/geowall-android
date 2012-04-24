317b21d0cb5cd005aa451ba7195bec9cb79f3098
toolanto <toolanto@gmail.com>
2012-04-24 17:58:16 +0200

    Sviluppata l'activity di login

---
+++ GeoWall/src/mobidev/geowall/GeoWallActivity.java
@@ -0,0 +1,13 @@
+package mobidev.geowall;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GeoWallActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
