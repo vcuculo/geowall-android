0d2600ce12943c4c543870badc3f0eb1ba65d6e5
Antonio Luca <toolanto@gmail.com>
2012-05-02 12:23:18 +0200

    registration layout

--- src/mobidev/geowall/Registration_AdvancedActivity.java
+++ src/mobidev/geowall/Registration_AdvancedActivity.java
@@ -8,15 +8,12 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.provider.MediaStore;
+
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+
 import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.ImageView;
