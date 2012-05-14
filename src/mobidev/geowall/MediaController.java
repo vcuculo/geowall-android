7359b8a07f02a735974299b289ac4941b36eaeb6
Antonio Luca <toolanto@gmail.com>
2012-05-14 15:13:47 +0200

    take photo

--- src/mobidev/geowall/MediaController.java
+++ src/mobidev/geowall/MediaController.java
@@ -53,6 +53,7 @@ public class MediaController {
 			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 			intent.putExtra(MediaStore.EXTRA_OUTPUT,
 					getOutputMediaFileUri(MEDIA_TYPE_IMAGE));
+			
 
 			return intent;
 
--- src/mobidev/geowall/RegistrationAdvancedActivity.java
+++ src/mobidev/geowall/RegistrationAdvancedActivity.java
@@ -1,6 +1,7 @@
 package mobidev.geowall;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Calendar;
@@ -266,12 +267,12 @@ public class RegistrationAdvancedActivity extends Activity implements
 					accountImage.setMaxHeight(40);
 					accountImage.setMaxWidth(40);
 					imageAccountFile = MediaController.getImage();
-					Bitmap b = BitmapFactory.decodeFile(imageAccountFile
-							.getPath());
+					//Bitmap b = BitmapFactory.decodeFile(imageAccountFile
+					//		.getPath());
 	
-					Bitmap showBitmap=b.createBitmap(b,0,0,40,40);
 					
-					accountImage.setImageBitmap(showBitmap);
+					
+					accountImage.setImageBitmap(decodeFile(imageAccountFile));
 					
 
 				}
@@ -285,6 +286,29 @@ public class RegistrationAdvancedActivity extends Activity implements
 
 	
 
+	}
+	
+	private Bitmap decodeFile(File f){
+	    try {
+	        //Decode image size
+	        BitmapFactory.Options o = new BitmapFactory.Options();
+	        o.inJustDecodeBounds = true;
+	        BitmapFactory.decodeStream(new FileInputStream(f),null,o);
+
+	        //The new size we want to scale to
+	        final int REQUIRED_SIZE=70;
+
+	        //Find the correct scale value. It should be the power of 2.
+	        int scale=1;
+	        while(o.outWidth/scale/2>=REQUIRED_SIZE && o.outHeight/scale/2>=REQUIRED_SIZE)
+	            scale*=2;
+
+	        //Decode with inSampleSize
+	        BitmapFactory.Options o2 = new BitmapFactory.Options();
+	        o2.inSampleSize=scale;
+	        return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+	    } catch (FileNotFoundException e) {}
+	    return null;
 	}
 
 }
