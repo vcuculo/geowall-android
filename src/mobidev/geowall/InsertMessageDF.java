62288be5fca5bf7ac4f12e3e08037e2455fa018a
Antonio Luca <toolanto@gmail.com>
2012-07-03 17:42:49 +0200

    insert image in text

--- src/mobidev/geowall/InsertMessageDF.java
+++ src/mobidev/geowall/InsertMessageDF.java
@@ -3,32 +3,36 @@ package mobidev.geowall;
 
 import java.io.File;
 
-import android.app.Activity;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class InsertMessageActivity extends Activity implements OnClickListener {
+public class InsertMessageDF extends DialogFragment implements OnClickListener {
 
+	
 	ImageButton upload;
 	TextView message;
-	String imageMessageBase64,text;
+	String imageMessageBase64;
 	
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.insertmessage);
-		ImageButton upload = (ImageButton) findViewById(R.id.uploadButton);
-		message = (TextView) findViewById(R.id.messageText);
-		upload.setOnClickListener(this);
-	
-	}
+
+
+	  @Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	            Bundle savedInstanceState) {
+	        View v = inflater.inflate(R.layout.insertmessage, container, false);
+	        return v;
+	    }
+
 	
 	@Override
 	public void onClick(View v) {
@@ -45,11 +49,10 @@ public class InsertMessageActivity extends Activity implements OnClickListener {
 			break;
 		}
 	}
+	
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 		if (requestCode == MediaController.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-		
-			if (resultCode == RESULT_OK) {
 
 				try {
 
@@ -65,18 +68,15 @@ public class InsertMessageActivity extends Activity implements OnClickListener {
 							.encodeBase64toString(MediaController.getImage());
 					Drawable imDr = new BitmapDrawable(temp);
 					imDr.setBounds(0, 0, 40, 40);
-					//text = "Entrato";
 					//setSharedPreference(message);
-					message.setCompoundDrawables(imDr, null, null, null);	
+					message.setCompoundDrawablesWithIntrinsicBounds(imDr, null, null, null);	
 					Log.i("DialogMetodo","Entrato");
 				} catch (Exception e) {
 					Log.i("Camera", "CameraError");
 					e.printStackTrace();
 				}
 
-			} else {
-				text = null;
-			}
+		
 
 		}
 	}
